01a8f735a7b82ac285a6166c71c838d5efb2513f
Anércio Ribeiro <anercio.ribeiro94@outlook.com>
2024-11-15 20:08:49 +0100

    Add Loading

--- src/hooks/getUser.ts
+++ src/hooks/getUser.ts
@@ -1,70 +1,7 @@
-
-// import { useQuery } from "@tanstack/react-query";
-// import { User } from "@/app/model/type";
-
 import { User } from "@/app/model/type";
 import { useQuery } from "@tanstack/react-query";
 import { redirect, useRouter } from "next/navigation";
 import { useEffect } from "react";
-
-// export function useUser() {
-//   const { data: user, isLoading: loading, isError, error } = useQuery<User | null>({
-//     queryKey: ["authUser"],
-//     queryFn: async () => {
-//       const response = await fetch("/api/getauthuser");
-//       if (!response.ok) throw new Error("Erro ao buscar o usuário");
-//       return response.json();
-//     },
-//     initialData: null,
-//   });
-
-//   if (isError) {
-//     console.error("Erro ao buscar o usuário:", error);
-//   }
-
-//   return { user, loading, error };
-// }
-
-
-
-
-// import { useQuery } from "@tanstack/react-query";
-// import { User } from "@/app/model/type";
-// import { useRouter } from "next/router";
-// import { useEffect, useState } from "react";
-
-// export function useUser() {
-//   const router = useRouter();
-//   const [mounted, setMounted] = useState(false); // Para verificar se o componente foi montado
-
-//   // Definindo o estado de autenticação e carregamento
-//   const { data: user, isLoading: loading, isError, error } = useQuery<User | null>({
-//     queryKey: ["authUser"],
-//     queryFn: async () => {
-//       const response = await fetch("/api/getauthuser");
-//       if (!response.ok) throw new Error("Erro ao buscar o usuário");
-//       return response.json();
-//     },
-//     initialData: null,
-//   });
-
-//   // Usando useEffect para garantir que o código roda no cliente
-//   useEffect(() => {
-//     setMounted(true); // Marca que o componente foi montado
-//   }, []);
-
-//   // Verifica se o componente foi montado e redireciona se houver erro
-//   useEffect(() => {
-//     if (mounted && isError) {
-//       console.error("Erro ao buscar o usuário:", error);
-//       router.push("/"); // Redireciona para a página inicial caso o usuário não esteja autenticado
-//     }
-//   }, [isError, error, mounted, router]);
-
-//   return { user, loading, error };
-// }
-
-
 
 export function useUser() {
   const router = useRouter();
